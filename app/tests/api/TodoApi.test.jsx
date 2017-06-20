1a840b89fa1b277940ce8a90444e8aa5b552e6bf
rmbarley <rmbarley@gmail.com>
2017-06-19 21:25:13 -0400

    Add filter

--- app/tests/api/TodoApi.test.jsx
+++ app/tests/api/TodoApi.test.jsx
@@ -47,6 +47,40 @@ describe('TodoApi', () => {
       var actualTodos = TodoApi.getTodos();
       expect(actualTodos).toEqual(todos);
     });
-
+    describe('filterTodos', () => {
+      var todos = [{
+        id: 1,
+        text: 'test',
+        completed: true
+      }, {
+        id: 2,
+        text: 'test',
+        completed: false
+      }, {
+        id: 3,
+        text: 'foo',
+        completed: true
+      }];
+      it('should return all items if showCompleted is true', () => {
+        var filteredTodos = TodoApi.filterTodos(todos, true, '');
+        expect(filteredTodos.length).toBe(3);
+      });
+      it('should return no items if showCompleted is false', () => {
+        var filteredTodos = TodoApi.filterTodos(todos, false, '');
+        expect(filteredTodos.length).toBe(1);
+      });
+      it('should sort by completed status', () => {
+        var filteredTodos = TodoApi.filterTodos(todos, true, '');
+        expect(filteredTodos[0].completed).toBe(false);
+      })
+      it('should filter todos by searchText', () => {
+        var filteredTodos = TodoApi.filterTodos(todos, true, 'test');
+        expect(filteredTodos.length).toBe(2);
+      })
+      it('should return all todos if searchText is empty', () => {
+        var filteredTodos = TodoApi.filterTodos(todos, true, '');
+        expect(filteredTodos.length).toBe(3);
+      })
+    });
   });
 });
